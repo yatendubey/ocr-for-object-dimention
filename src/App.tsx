4315feca730fc8f66f8fac1f3478f5598c957310
yatendubey <50910030+yatendubey@users.noreply.github.com>
2020-05-21 17:08:17 +0530

    initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render(){
+    return(
+      <div>Home</div>
+    )
+  }
+ }
+
+export default App;
